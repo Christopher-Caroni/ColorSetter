e8cd06c9101b52addef54b677456170fba36aa4c
Hexafile <thomas.plouchart@hotmail.fr>
2016-05-20 14:39:08 +0200

    Initialize butoon and list

--- src/Couleur.java
+++ src/Couleur.java
@@ -1,4 +1,8 @@
+import java.awt.Color;
 
 public class Couleur {
-
+	
+	public Couleur(){
+		
+	}
 }
--- src/Principle.java
+++ src/Principle.java
@@ -1,4 +1,44 @@
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Principle {
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
+public class Principle extends JFrame {
+
+	List<Couleur> colors = new ArrayList<Couleur>();
+	
+	public Principle() {
+		colors.add(new Couleur());
+		
+		setLayout(new GridLayout(11,1));;
+		JButton b = new JButton("Ajouter une couleur");
+		
+		b.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				colors.add(new Couleur());
+				for(Couleur c : colors){
+					add(c);
+				}		
+			}
+		});
+		
+		add(b);
+		
+
+		setSize(new Dimension(500,500));
+		setTitle("Color setter");
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		setLocation((int) ((width/2 - getWidth()/2)), (int) (height/2 - getHeight()/2));
+		setVisible(true);
+		
+	}
 }
