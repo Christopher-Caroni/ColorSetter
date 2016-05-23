5089181776013c834b01911a3fecaa339859fc9d
Christopher-Caroni <christophercaroni97@gmail.com>
2016-05-23 09:29:40 +0200

    arraylist et min size

--- src/Principle.java
+++ src/Principle.java
@@ -6,6 +6,8 @@ import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -16,14 +18,17 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.Border;
 
+@SuppressWarnings("serial")
 public class Principle extends JFrame{
 
 	private JPanel panel;
+	private List<Couleur> list;
 	
 	public Principle() {
 		
 		panel = new JPanel();
 		panel.setLayout(new GridLayout(4,  1));
+		list = new ArrayList<>();
 				
 		setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 		JButton button = new JButton("Ajouter une couleur");
@@ -33,9 +38,15 @@ public class Principle extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				if (panel.getComponentCount() < 11) {
-					panel.add(new Couleur());
-					panel.setPreferredSize(new Dimension(850, 74 * (panel.getComponentCount()+2) ));	
-					panel.setLayout(new GridLayout(panel.getComponentCount()+2, 1));
+					list.add(new Couleur());
+					panel.add(list.get(list.size()-1));
+					if (panel.getComponentCount() > 3) {
+						panel.setPreferredSize(new Dimension(800, 74 * panel.getComponentCount()));
+						panel.setLayout(new GridLayout(panel.getComponentCount(), 1));
+					} else {
+						panel.setPreferredSize(new Dimension(800, 74 * (panel.getComponentCount() + 1)));
+						panel.setLayout(new GridLayout(panel.getComponentCount() + 1, 1));
+					}
 					validate();
 					repaint();
 				}
@@ -43,8 +54,9 @@ public class Principle extends JFrame{
 		});
 		JLabel legend = new JLabel("                            Selectionner une couleur                               Couleur       Niveau de gris                    Code RGB");
 		panel.add(legend);
-		panel.add(new Couleur());
-		panel.setPreferredSize(new Dimension(850, 74 * panel.getComponentCount()));
+		list.add(new Couleur());
+		panel.add(list.get(0));
+		panel.setPreferredSize(new Dimension(800, 74 * panel.getComponentCount()));
 		JScrollPane scroll = new JScrollPane(panel);
 		Border border = BorderFactory.createTitledBorder("Liste des couleurs");
 	    scroll.setBorder(border);
@@ -53,6 +65,7 @@ public class Principle extends JFrame{
 		add(button);
 
 		setSize(new Dimension(850, 400));
+		setMinimumSize(getSize());
 		setTitle("Color setter");
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		double width = screenSize.getWidth();
